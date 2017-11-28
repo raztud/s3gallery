58351c302bca597f529ec1fa7f11a556cb4fc289
Răzvan Tudorică <razvan.tudorica@avira.com>
2017-11-28 18:36:42 +0200

    friendly url

--- browser/urls.py
+++ browser/urls.py
@@ -4,5 +4,5 @@ from browser.views import IndexView, ShowFileView
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^show-file/$', ShowFileView.as_view(), name='show-file'),
-    url(r'^file/(?P<element>[a-zA-Z_0-9\-\/\.,]+)/$', ShowFileView.as_view(), name='file'),
+    url(r'^file/(?P<element>[a-zA-Z_0-9\-\/\.,]+)$', ShowFileView.as_view(), name='file'),
 ]
