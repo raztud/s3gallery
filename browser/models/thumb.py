7076dd27c2c038bb96a2aa0f3ae6c4dd1de97494
Răzvan Tudorică <razvan.tudorica@avira.com>
2017-12-21 13:13:19 +0200

    md5sum should not be unique

--- browser/models/thumb.py
+++ browser/models/thumb.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Thumb(models.Model):
     s3url = models.CharField(db_index=True, max_length=500, blank=False, null=False)
-    md5sum = models.CharField(db_index=True, unique=True,
+    md5sum = models.CharField(db_index=True,
                               max_length=255,
-                              blank=False, null=False)
+                              blank=False,
+                              null=False)
     thumb_path = models.TextField(blank=False, null=False)
 
     def __str__(self):
